e20a920fdaf95f20d5ff93829098e31713205e1d
Abhishek Yadav <abhishekcms611@gmail.com>
2018-04-09 16:50:23 +0530

    Added face recognition

---
+++ faceCNN.py
@@ -0,0 +1,109 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import os, glob
+import cv2
+
+
+# In[18]:
+
+
+
+
+# In[2]:
+
+
+import numpy as np
+import pandas
+from keras.utils import np_utils
+import matplotlib.pyplot as plt
+from keras.utils import to_categorical
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
+from keras.layers import Flatten
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.cross_validation import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+from keras.datasets import cifar10
+import keras
+from PIL import Image
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.constraints import maxnorm
+from keras import regularizers
+
+
+# In[3]:
+
+
+
+# In[4]:
+
+
+X=[]
+Y=np.zeros(52,dtype='int32')
+i=0
+for infile in glob.glob( os.path.join('shubh/', "*.jpg") ):
+    im = cv2.imread(infile)
+    #gray_image = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    x=np.array(im)
+    X.append(list(x))
+    Y[i]=int(0)   
+    i=i+1
+for infile in glob.glob( os.path.join('abhi/', "*.jpg") ):
+    im = cv2.imread(infile)
+    #gray_image = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    x=np.array(im)
+    X.append(list(x))
+    Y[i]=int(1)
+    i=i+1
+
+# In[5]:
+
+
+X=np.array(X)
+X=X/255
+#print X.shape
+#type(Y[0])
+#for i in range(52):
+#    if(Y[i]==1):
+#        Y[i]=0.99
+#        Y[i]=int(Y[i])
+
+
+# In[ ]:
+
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(20, 20), strides=(1, 1),
+                 activation='relu',
+                 input_shape=[50,50,3],kernel_initializer='random_uniform'))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+model.add(Flatten())
+model.add(Dense(100, activation='relu',kernel_initializer='random_uniform'))
+model.add(Dense(50, activation='relu',kernel_initializer='random_uniform'))
+model.add(Dense(1, activation='sigmoid',kernel_initializer='random_uniform'))
+model.compile(loss=keras.losses.logcosh,
+              optimizer='adam',
+              metrics=['accuracy'])
+model.fit(X,Y,epochs=50)
+
+
+# In[22]:
+
+
+im=cv2.imread('predict.jpg')
+x=[]
+x.append(np.array(im))
+x=np.array(x)
+x=x/255
+y=model.predict(x)
+print y
+
